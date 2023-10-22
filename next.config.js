6ad0e256ff995b7cd02e05b09bf8c77af712433a
Fatih Talha Özdemir <fatihtalhaozdemir@gmail.com>
2023-10-23 06:29:31 +0800

    Vercel configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const { get } = require("@vercel/edge-config");
+const { withContentlayer } = require("next-contentlayer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async rewrites() {
+	experimental: {
+		serverActions: true,
+	},
+	serverRuntimeConfig: {
+		PROJECT_ROOT: __dirname,
+	},
+	redirects() {
 		return [
 			{
 				source: "/:path*",
@@ -10,4 +19,15 @@ const nextConfig = {
 	},
 };
 
-module.exports = nextConfig;
+// https://nextjs.org/docs/advanced-features/security-headers
+const ContentSecurityPolicy = `
+    default-src 'self' vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self' data:;
+`;
+
+module.exports = withContentlayer(nextConfig);
