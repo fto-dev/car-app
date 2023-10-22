6315a96ebbace0590e0e237eb2ea4ea18d958f20
Fatih Talha Özdemir <fatihtalhaozdemir@gmail.com>
2023-10-22 15:47:34 +0800

    installed react router, next react-router configuration rewrites, added pages,  added header component, style cleanup

--- app/page.js
+++ app/page.js
@@ -1,19 +1,29 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Header from "@/components/header";
+
+import Mainpage from "@/components/home";
+import Favorites from "@/components/favorites";
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<div>
-				<Image
-					src="/coming-soon.png"
-					alt="Vercel Logo"
-					width={400}
-					height={400}
-					priority
-				/>
-			</div>
+			<Router>
+				<Header Link={Link} />
+				<Routes>
+					<Route
+						path="/"
+						element={<Mainpage />}
+					/>
+					<Route
+						path="favorites"
+						element={<Favorites />}
+					/>
+				</Routes>
+			</Router>
 		</main>
 	);
 }
---
+++ components/header.js
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function Header({ Link }) {
+	return (
+		<Navbar
+			expand="lg"
+			className="bg-body-tertiary"
+		>
+			<Container>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Link
+							className="nav-link"
+							to="/"
+						>
+							Home
+						</Link>
+						<Link
+							className="nav-link"
+							to="/favorites"
+						>
+							Your Favorite Cars (56)
+						</Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default Header;
