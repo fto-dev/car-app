73aa6b503026f0a46d5b1b156e6818ab46ef94c5
Fatih Talha Özdemir <fatihtalhaozdemir@gmail.com>
2023-10-23 12:55:35 +0800

    Revert "Vercel configuration"
    This reverts commit 6ad0e256ff995b7cd02e05b09bf8c77af712433a.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-const { get } = require("@vercel/edge-config");
-const { withContentlayer } = require("next-contentlayer");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverActions: true,
-	},
-	serverRuntimeConfig: {
-		PROJECT_ROOT: __dirname,
-	},
-	redirects() {
+	async rewrites() {
 		return [
 			{
 				source: "/:path*",
@@ -19,15 +10,4 @@ const nextConfig = {
 	},
 };
 
-// https://nextjs.org/docs/advanced-features/security-headers
-const ContentSecurityPolicy = `
-    default-src 'self' vercel.live;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
-    style-src 'self' 'unsafe-inline';
-    img-src * blob: data:;
-    media-src 'none';
-    connect-src *;
-    font-src 'self' data:;
-`;
-
-module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig;
