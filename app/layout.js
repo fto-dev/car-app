b3285721a1cd81fd15cf13b491382d411e2274b3
Fatih Ozdemir <fatihtalhaozdemir@gmail.com>
2025-10-16 13:58:16 +0700

    analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Analytics />
+				{children}
+				</body>
 		</html>
 	);
 }
